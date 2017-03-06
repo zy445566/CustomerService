124594286ffb78ce2ccaa60e42187a6a377733dc
ZhangYuan <zyct@vip.qq.com>
2017-03-06 18:52:25 +0800

    update。。。
    update。。。

--- test.js
+++ test.js
@@ -1,24 +1,28 @@
 const CustomerService = require("./index");
 
-var customerService = new CustomerService('myTest');
+var customerService = new CustomerService();
 
 var sw = customerService.getSplitWord();
 var ie = customerService.getImportExport();
 var qa = customerService.getQuestionAnswer();
 
 // ie.printData({start:'word',limit:10});
-// sw.collisionWord(sw.sentenceToList('你是屌丝吗'))
+// sw.collisionWord(sw.sentenceToList('我在洗澡，你在干嘛'))
 // .then((res)=>{
 // 	console.log(res);
 // });
 
-// ie.printData({start:'',limit:30});
-// ie.readWordToLevel('word1.log','word');
 
-// ie.readWordToLevel('qa1.log','qa');
+// ie.printData({start:'',limit:30});
+// ie.readWordToLevel('word.log','word');
+
+// ie.readWordToLevel('qa.log','qa');
 
 
 qa.getAnswer('你是屌丝吗')
+.then((res)=>{
+	console.log(res);
+})
 .catch((err)=>{
 	console.log(err);
 });
